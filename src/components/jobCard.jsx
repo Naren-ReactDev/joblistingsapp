a45b34dde7e0d1968411781d9e572904564e6ac9
naren <naren.thatiknda@gmail.com>
2024-02-22 14:58:20 +0000

    implemented filter functionality.

--- src/components/jobCard.jsx
+++ src/components/jobCard.jsx
@@ -1,9 +1,9 @@
 import * as CompanyLogos from "./companyLogo";
-const JobCard = ({ jobRecord }) => {
+const JobCard = ({ jobRecord,updateFilterTags }) => {
 
   const getJobTags = (tags) => {
     return tags.map((tag,key) => {
-      return <p className="jobTag"> {tag} </p>;
+      return <p className="jobTag" onClick={()=>updateFilterTags(tag)}> {tag} </p>;
     });
   };
 
@@ -30,15 +30,14 @@ const JobCard = ({ jobRecord }) => {
             </div>
             <div className="jobRole">{jobRecord?.position}</div>
             <div className="jobDetails2">
-              {jobRecord?.postedAt} . {jobRecord?.contract} .
-              {jobRecord?.location}
+              {jobRecord?.postedAt}  .  {jobRecord?.contract}  .  {jobRecord?.location}
             </div>
           </div>
         </div>
       </div>
       <div className="rightSection">
-        {jobRecord?.role ? <p className="jobTag">{jobRecord.role}</p> : ""}
-        {jobRecord?.level ? <p className="jobTag">{jobRecord.level}</p> : ""}
+        {jobRecord?.role ? <p className="jobTag" onClick={()=>updateFilterTags(jobRecord.role)}>{jobRecord.role}</p> : ""}
+        {jobRecord?.level ? <p className="jobTag" onClick={()=>updateFilterTags(jobRecord.level)}>{jobRecord.level}</p> : ""}
         {getJobTags(jobRecord.languages)}
       </div>
     </div>
--- src/components/jobListings.jsx
+++ src/components/jobListings.jsx
@@ -1,12 +1,88 @@
 import data from "../data.json";
 import JobCard from "./jobCard";
+import { useMemo } from "react";
+import { useState } from "react";
 
 export const JobListings = () => {
-  const getJobcard = () => {
-    return data.map((jobRecord, key) => {
-      return <JobCard jobRecord={jobRecord}></JobCard>;
-    });
+  const [filtertags, setFiltertags] = useState([]);
+
+  const updateFilterTags = (tag) => {
+    if (!filtertags.includes(tag)) {
+      setFiltertags((preState) => {
+        return [...preState, tag];
+      });
+    }
   };
 
-  return <>{getJobcard()}</>;
+  const removeFiltertags = (tag) => {
+    setFiltertags(
+      tag === "Clear" ? [] : filtertags.filter((item) => item !== tag)
+    );
+  };
+
+  const filterFlag = (jobRecord) => {
+    const singleJobTags = [
+      ...jobRecord.languages,
+      jobRecord.role,
+      jobRecord.level,
+    ];
+    return filtertags.every((item) => singleJobTags.includes(item));
+    // return filtertags?.find((item) => {
+    //   return singleJobTags.includes(item);
+    // });
+  };
+
+  const displayFilterTags = useMemo(() => {
+    return filtertags.map((tag, key) => {
+      return (
+        <>         
+          <p className="filterTag">{tag}</p>
+          <button
+            className="removeFiltorButton"
+            onClick={() => removeFiltertags(tag)}
+          >
+            X
+          </button>
+        </>
+      );
+    });
+  }, [filtertags,removeFiltertags]);
+
+  const getJobcard = () => {
+    const filteredData = data.filter((record) => {
+      if (filtertags.length === 0 || filterFlag(record)) {
+        return record;
+      }
+    });
+
+    return filteredData.map((jobRecord, key) => {
+      return (
+        <JobCard
+          jobRecord={jobRecord}
+          updateFilterTags={updateFilterTags}
+        ></JobCard>
+      );
+    });
+  };
+  const displayFilterBar = useMemo(() => {
+    return filtertags.length === 0 ? (
+      <></>
+    ) : (
+      <div className="filterBar">
+        <div className="filterBarLeft">{displayFilterTags}</div>
+        <div>
+          <p className="clearFilters" onClick={() => removeFiltertags("Clear")}>
+            Clear
+          </p>
+        </div>
+      </div>
+    );
+  }, [filtertags, removeFiltertags]);
+
+  return (
+    <>
+      <div className="header">{displayFilterBar}</div>
+      <div className="mainBody">{getJobcard()}</div>
+    </>
+  );
 };
